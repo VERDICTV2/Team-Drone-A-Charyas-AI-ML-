a855262c2141560dff58a297495bd9bbf215bf15
Abhinava Teja <102428571+VERDICTV2@users.noreply.github.com>
2022-11-26 02:22:55 +0530

    Update train_mod.py

--- train_mod.py
+++ train_mod.py
@@ -1,27 +1,82 @@
 
-from detecto import core, utils, visualize
-from detecto.visualize import show_labeled_image, plot_prediction_grid
-from torchvision import transforms
-import matplotlib.pyplot as plt
-import numpy as np
 from PIL import Image
-
-custom_transforms = transforms.Compose([
-transforms.ToPILImage(),
-transforms.Resize(900),
-transforms.RandomHorizontalFlip(0.5),
-transforms.ColorJitter(saturation=0.2),
-transforms.ToTensor(),
-utils.normalize_transform(),
-])
+import os
+import cv2
+import numpy as np
+from detecto import core, utils, visualize
 
 
+# assign directory
 
-Train_dataset=core.Dataset("D:\\Edhitha 2023\\new_seg\\train",transform=custom_transforms)#L1
-Test_dataset = core.Dataset("D:\\Edhitha 2023\\new_seg\\test")#L2
-loader=core.DataLoader(Train_dataset, batch_size=2, shuffle=True)#L3
-model = core.Model(['Target'])#L4
-losses = model.fit(loader, Test_dataset, epochs=25, lr_step_size=5, learning_rate=0.001, verbose=True)#L5
+# iterate over files i
+        # checking if it is a file
+        #if os.path.isfile(f):
 
-model.save('model_weights.pth')
-model = core.Model.load('model_weights.pth', ['Target']) 
+
+
+'''ilo =0 
+for filename in os.listdir("C:\\Users\\spoor\\Desktop"):
+
+            f=os.path.join("C:\\Users\\spoor\\Desktop\\totest",filename)
+            image = utils.read_image(f)
+            predictions =  core.Model.load('model_weights.pth', ['human']).predict(image)
+                #print(predictions)
+            labels, boxes, scores = predictions
+                #print(boxes)
+                #show_labeled_image(image, boxes, labels)
+            thresh=0.3
+            filtered_indices=np.where(scores>thresh)
+            filtered_scores=scores[filtered_indices]  
+            filtered_boxes=boxes[filtered_indices]
+            x = filtered_boxes.tolist()
+            print(x)
+            num_list = filtered_indices[0].tolist()
+            filtered_labels = [labels[ilo] for ilo in num_list]
+                #show_labeled_image(image, filtered_boxes, filtered_labels)
+            img = Image.open(f)
+                # print(x[0][0])
+            area = (x[0][0], x[0][1], x[0][2], x[0][3])
+            cropped_img = img.crop(area)
+            cropped_img 
+            cropped_img.save("D:\\Edhitha 2023\\new_seg\\results\\result_img{}.jpg".format(ilo))
+            ilo = ilo +1 '''
+
+image = utils.read_image('C:\\Users\\spoor\\Desktop\\cam_1_2_and_helf_mins_00000063.jpg')
+model=core.Model.load("C:\\Users\\spoor\\Desktop\\model_weights.pth",["human"])
+predictions = model.predict(image)
+print(predictions)
+labels, boxes, scores = predictions
+print(boxes)
+show_labeled_image(image, boxes, labels)
+       
+thresh=0.55
+filtered_indices=np.where(scores>thresh)
+filtered_scores=scores[filtered_indices]
+filtered_boxes=boxes[filtered_indices]
+x = filtered_boxes.tolist()
+count = 0
+while(len(x)!=0):
+  print(x)
+  print(x[0])
+  num_list = filtered_indices[0].tolist()
+  filtered_labels = [labels[i] for i in num_list]
+  show_labeled_image(image, filtered_boxes, filtered_labels)
+  break
+
+# Given information
+img = Image.open("C:\\Users\\spoor\\Desktop\\cam_1_2_and_helf_mins_00000063.jpg")
+#width, height = 440, 190
+#x, y = 100, 20
+ 
+# Select area to crop
+area = (x[0][0], x[0][1], x[0][2], x[0][3])
+x_cp = (x[0][0] + x[0][2])/2
+y_cp = (x[0][1] + x[0][3])/2
+print(x_cp,",",y_cp)
+ 
+# Crop, show, and save image
+cropped_img = img.crop(area)
+#img.show(cropped_img)
+cropped_img.save("C:\\Users\\spoor\\Desktop\\out\\cropped_image.jpg")
+im=image.open("C:\\Users\\spoor\\Desktop\\out\\cropped_image.jpg")
+im.show()
